9fdf2b2d46949d22121f4910302d1350e2ba9f2d
Pablo Moreno <pablo.a.moreno@gmail.com>
2014-10-02 14:18:30 +0100

    Adds the protein name functionality (needs to be refactored though)

--- R/EuclideanClusterer.R
+++ R/EuclideanClusterer.R
@@ -16,13 +16,26 @@ EuclideanClusterer <- setClass("EuclideanClusterer",
 #'   
 #' @export
 setMethod("plotHeatMap",signature(object="EuclideanClusterer"),
-          function(object,displayProt=FALSE,displayFeat=FALSE) {
-            protLab = if(displayProt) rownames(object@uniqueFeaturesTable) else c("");
+          function(object,displayProt=FALSE,displayFeat=FALSE,useProtNames=TRUE,title="") {
+            protLab = c("")
+            if(displayProt) { 
+              if(useProtNames) {
+                # first retrieve only the first name (make a new data table)
+                object@annotation[,list(GENE=unlist(strsplit(GENES,split=' '))[1]),by=UNIPROTKB]->id2names
+                # This part should be used by both the Euclidean and Presence absence
+                protLab <- id2names[match(rownames(object@uniqueFeaturesTable),id2names$UNIPROTKB),GENE,with=T]
+              } else {
+                protLab = rownames(object@uniqueFeaturesTable)
+              }
+            }
+              
+              
             featLab  = if(displayFeat)  colnames(object@uniqueFeaturesTable) else c("");
             heatmap.2(as.matrix(object@uniqueFeaturesTable),
                       Rowv=object@colorProtDend,
                       Colv=as.dendrogram(object@clustFeatures),
                       col=colorpanel(99,"lightgrey",mid="purple",high="blue"),breaks=100,
-                      trace="none",key=FALSE,labRow=protLab,labCol=featLab)->hm
+                      trace="none",key=FALSE,labRow=protLab,labCol=featLab,main=title
+                      )->hm
             return(hm)
           })
