7640a2959146e652fe1f9ad40cd46f7d73c0bbd9
Pablo Moreno <pablo.a.moreno@gmail.com>
2014-10-10 10:15:24 +0100

    Removes missing cases when using a minimum number of features.

--- R/PresenceAbsenceClusterer.R
+++ R/PresenceAbsenceClusterer.R
@@ -13,10 +13,12 @@ setMethod("getColourModel",signature(object="PresenceAbsenceClusterer"),
 setMethod("calculateDistances",signature(object="PresenceAbsenceClusterer"), function(object,minFeaturePres=0) {
   toDrop <- names(which(colSums(object@uniqueFeaturesTable)<minFeaturePres))
   uniqTableFeatsAboveMin <- object@uniqueFeaturesTable[!(colnames(object@uniqueFeaturesTable) %in% toDrop)]
+  toDropProts <- names(which(rowSums(uniqTableFeatsAboveMin)==0))
+  object@uniqueFeaturesTable<-uniqTableFeatsAboveMin[!(rownames(uniqTableFeatsAboveMin) %in% toDropProts),]
   # here we should use jaccard or other binary distance for presence/absence, not euclidean.
   # 1 if for jaccard
-  dist.binary(df=as.matrix(uniqTableFeatsAboveMin),method=1)->object@proteinDist
-  dist.binary(df=t(as.matrix(uniqTableFeatsAboveMin)),method=1)->object@featureDist
+  dist.binary(df=as.matrix(object@uniqueFeaturesTable),method=1)->object@proteinDist
+  dist.binary(df=t(as.matrix(object@uniqueFeaturesTable)),method=1)->object@featureDist
   # According to this paper http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=6421371&tag=1 complete and ward are 
   # the best options for binary data
   hclust(d=object@proteinDist,method="complete")->object@clustProts
